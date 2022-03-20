1948318bec93e23d2c7b77f1e3a0d47c59a73d14
tranthaibao29112001 <75375136+tranthaibao29112001@users.noreply.github.com>
2022-03-20 21:24:05 +0700

    Thêm tính năng tra cứu lịch sử và tính năng tìm kiếm theo định nghĩa

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
     static HashMap<String,SlangWord> slangWords = new HashMap<String,SlangWord>();
+    static ArrayList<SlangWord> slangWordArrayList = new ArrayList<>();
+    static ArrayList<SlangWord> slangSearchHistory =  new ArrayList<>();
+    static HashMap<String,List<SlangWord>> definitionSearchHistory = new HashMap<String,List<SlangWord>>();
     public static void main(String[] args) throws IOException {
         initSlangWords();
         boolean isContinue = true;
@@ -26,16 +34,43 @@ public class Main {
                         }
                         else{
                             System.out.println(slangWord);
+                            slangSearchHistory.add(slangWord);
                         }
-                        System.out.println();
-                        System.out.println("Nhấn Enter để tiếp tục.");
-                        br.readLine();
                         break;
                     }
                     case 2:{
+                        System.out.println("Nhập định nghĩa:");
+                        str = br.readLine();
+                        CharSequence charSequence = str;
+                        Predicate<SlangWord> definitionFilter =  slangWord -> slangWord.getDefinition().contains(charSequence);
+                        List<SlangWord> filterList = slangWordArrayList.stream().filter(definitionFilter).collect(Collectors.toList());
+                        for (int i=0;i<filterList.size();i++){
+                            System.out.println(filterList.get(i));
+                        }
+                        definitionSearchHistory.put(str,filterList);
+
                         break;
                     }
                     case 3:{
+                        System.out.println("1. Tìm theo SlangWord");
+                        System.out.println("2. Tìm theo Definition");
+                        str = br.readLine();
+                        switch (Integer.parseInt(str)){
+                            case 1:{
+                                for (int i=0;i<slangSearchHistory.size();i++){
+                                    System.out.println(i+1 +" - " +slangSearchHistory.get(i));
+                                }
+                                break;
+                            }
+                            case 2:{
+                                int i = 0;
+                                for(String key:definitionSearchHistory.keySet()){
+                                    System.out.println(i+1+" - " + key+" - "+definitionSearchHistory.get(key));
+                                    i++;
+                                }
+                                break;
+                            }
+                        }
                         break;
                     }
                     case 4:{
@@ -54,10 +89,13 @@ public class Main {
                         break;
                     }
                 }
+                System.out.println("Nhấn Enter để tiếp tục.");
+                br.readLine();
             }
             catch (Exception exception){
                 continue;
             }
+
         }
     }
     public static void printMenu(){
@@ -84,6 +122,7 @@ public class Main {
             }
             else{
                 SlangWord slangWord = convertStringToSangWord(str);
+                slangWordArrayList.add(slangWord);
                 slangWords.put(slangWord.getSlangWord(),slangWord);
             }
         }
